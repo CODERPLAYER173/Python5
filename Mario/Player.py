81bad454662a8f02f24dc2c27c7792a2ead31c3e
Dwarfy02-pro <18191082.dpsgv@dpsgs.org>
2026-03-10 18:16:47 +0530

    m

---
+++ Mario/Player.py
@@ -0,0 +1,4 @@
+from turtle import Turtle as Tu
+
+class Player(Tu):
+    pl
--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -9,14 +9,23 @@ turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 state = data["state"].tolist()
 score = 0
+guesed_states = []
+
 while score <= 50:
 
     b = turtle.textinput(f'guess the State your score is {score}', "What is the name of the specific state")
+    if b == "Exit":
+        not_guessed = [ x for x in state if x not in guesed_states]
+        datas = pandas.DataFrame(not_guessed)
+        datas.to_csv("Not_guessed.cvs")
+        print(not_guessed)
     if  b in state:
+        guesed_states.append(b)
         score += 1
         x = data.x[data.state == b].tolist()
         y = data.y[data.state == b].tolist()
         text.hideturtle()
         text.teleport(x[0],y[0])
         text.write(b,move=False,align="center",font=("Arial",20,"bold"))
+
 turtle.mainloop()
