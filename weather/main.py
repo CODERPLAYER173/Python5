40d08d9e89265b63a446cb88f10d40665b6541da
Dwarfy02-pro <18191082.dpsgv@dpsgs.org>
2026-05-15 18:38:00 +0530

    ee
    eeeeeee

--- weather/main.py
+++
@@ -1,9 +0,0 @@
-
-import pandas
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-fur_color = data.PrimaryFurColor
-print(fur_color)
-count = fur_color.count()
-datas = pandas.DataFrame(fur_color,count)
-datas.to_csv("newfile.csv")
-
